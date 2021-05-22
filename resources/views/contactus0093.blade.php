d2f9c4af189e6b70a6cd85c0c553a6832f20b5fc
fahmiizzulhaq <84610811+fahmiizzulhaq@users.noreply.github.com>
2021-05-22 13:02:27 +0700

    Add files via upload

---
+++ resources/views/contactus0093.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact US</title>
+</head>
+<body>
+    <img src="images/img.jpeg" size="small" class="card-img-top" style="max-width: 18rem";>
+    <p>nama : Fahmi Izzulhaq</p>
+    <p>NBI : 1461900093</p>
+    <p>kontak(WA) : +62895609794405</p>
+    <p>Email : fahmiizzulhaq10@gmail.com</p>
+    <p>Instagram : izzulhaqfahmi</p>
+</body>
+</html>
