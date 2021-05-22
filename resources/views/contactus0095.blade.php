d59ae1e9f4cf13e45c359042cb4d9df798c19aed
Muhammad Reza Oktaviangga <84327523+rezz24@users.noreply.github.com>
2021-05-22 13:20:38 +0700

    Update and rename contactus0093.blade.php to contactus0095.blade.php

---
+++ resources/views/contactus0095.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact US</title>
+</head>
+<body>
+    <img src="images/img.jpeg" size="small" class="card-img-top" style="max-width: 18rem";>
+    <p>nama : Muhammad Reza Oktaviangga</p>
+    <p>NBI : 1461900095</p>
+    <p>kontak(WA) : +628989367099</p>
+    <p>Email : m.rezaoktaviangga101@gmail.com</p>
+    <p>Instagram : m_oktaviangga</p>
+</body>
+</html>
