5f95262dde48e9f9acce85205f06150a0a21c998
kRuvpin <82986902+kRuvpin@users.noreply.github.com>
2021-05-22 10:41:36 +0700

    Add files via upload

---
+++ resources/views/home0083.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+	<title>HOME</title>
+</head>
+<body background="images/cover.jpg">
+<div id="header" class="header">
+		<div id="search">	
+</div>	  
+        <h1>INI HALAMAN HOME</h1>
+		<li class="first"><a href="/contactus0083">Contact US</a></li>
+		<li class="first"><a href="/artikel0083">Artikel</a></li>
+</body>
+</html>
